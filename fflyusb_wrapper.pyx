226e65d3edb70995292cbd7a780707b360cc8e9f
Bohdan Turani <bodya00@list.ru>
2019-05-16 10:06:09 +0200

    Move enums to __init__ from __cinit__

--- fflyusb_wrapper.pyx
+++ fflyusb_wrapper.pyx
@@ -96,7 +96,7 @@ cdef extern from *:
             bool ConfigureReelsEx(char numberOfReels, int halfStepsPerTurn, char stepsPerSymbol)
             bool SetOutputBrightness(char brightness)
 
-            bool SetConfig(usbSerialPort port, _DCB* config, usbPortType ptype)
+            bool SetConfig(usbSerialPort port, _DCB*config, usbPortType ptype)
             # CCTalk Mode 1 and RS232 Polled Mode
             bool ConfigureCCTalkPort(usbSerialPort port, CCTalkConfig *cctalkConfig)
             bool SetPolledHostTimeout(usbSerialPort port, unsigned char*deviceNumber, double timeout)
@@ -139,6 +139,8 @@ cdef class PyFireFlyUSB:
 
     def __cinit__(self):
         self.thisptr = new FireFlyUSB()
+
+    def __init__(self):
         self.usbSerialPort = {
             'PORT_A': PORT_A,
             'PORT_B': PORT_B,
@@ -160,234 +162,232 @@ cdef class PyFireFlyUSB:
             'PORT_MDB_SLAVE': PORT_MDB_SLAVE
         }
 
-    def __dealloc__(self):
-        del self.thisptr
+def __dealloc__(self):
+    del self.thisptr
 
     # System Functions.
 
     #
-    def init(self, int BoardNumber):
-        return self.thisptr.init(BoardNumber)
+def init(self, int BoardNumber):
+    return self.thisptr.init(BoardNumber)
 
     #
-    def close(self):
-        return self.thisptr.close()
+def close(self):
+    return self.thisptr.close()
 
+def GetRawInputs(self):
+    cdef usbInput temp
+    success = self.thisptr.GetRawInputs(&temp)
+    return temp.byIn[0], temp.byIn[1], temp.byIn[2]
 
-    def GetRawInputs(self):
-        cdef usbInput temp
-        success = self.thisptr.GetRawInputs(&temp)
-        return temp.byIn[0], temp.byIn[1], temp.byIn[2]
+def GetChangedInputs(self):
+    cdef usbInput temp
+    success = self.thisptr.GetChangedInputs(&temp)
+    return temp.byIn[0], temp.byIn[1], temp.byIn[2]
 
-    def GetChangedInputs(self):
-        cdef usbInput temp
-        success = self.thisptr.GetChangedInputs(&temp)
-        return temp.byIn[0], temp.byIn[1], temp.byIn[2]
-
-    def GetInputs(self):
-        cdef usbInput temp
-        success = self.thisptr.GetInputs(&temp)
-        return temp.byIn[0]
+def GetInputs(self):
+    cdef usbInput temp
+    success = self.thisptr.GetInputs(&temp)
+    return temp.byIn[0]
     # I an attempt to make things more pythonic we use the wrapper to do
     # the housekeeping, i.e. changing the returned bytearray into a Pyhon string.
 
     #
-    def GetFittedBoard(self):
-        temp = bytearray(4)
-        success = self.thisptr.GetFittedBoard(temp)
-        self.FittedBoard = temp[0]
-        del temp
-        return success
+def GetFittedBoard(self):
+    temp = bytearray(4)
+    success = self.thisptr.GetFittedBoard(temp)
+    self.FittedBoard = temp[0]
+    del temp
+    return success
 
     #
-    def GetBoardSpeed(self):
-        temp = bytearray(4)
-        success = self.thisptr.GetBoardSpeed(temp)
-        self.BoardSpeed = temp[0]
-        del temp
-        return success
+def GetBoardSpeed(self):
+    temp = bytearray(4)
+    success = self.thisptr.GetBoardSpeed(temp)
+    self.BoardSpeed = temp[0]
+    del temp
+    return success
 
     #
-    def GetProductVersion(self):
-        temp = bytearray(20)
-        success = self.thisptr.GetProductVersion(temp)
-        self.ProductVersion = temp.decode('utf-8').strip('\x00')
-        del temp
-        return success  # {'ProductVersion', self.ProductVersion}
+def GetProductVersion(self):
+    temp = bytearray(20)
+    success = self.thisptr.GetProductVersion(temp)
+    self.ProductVersion = temp.decode('utf-8').strip('\x00')
+    del temp
+    return success  # {'ProductVersion', self.ProductVersion}
 
     #
-    def GetDllVersion(self):
-        temp = bytearray(20)
-        success = self.thisptr.GetDllVersion(temp)
-        self.DllVersion = temp.decode('utf-8').strip('\x00')
-        del temp
-        return success  # {'DllVersion', self.DllVersion}
+def GetDllVersion(self):
+    temp = bytearray(20)
+    success = self.thisptr.GetDllVersion(temp)
+    self.DllVersion = temp.decode('utf-8').strip('\x00')
+    del temp
+    return success  # {'DllVersion', self.DllVersion}
 
     #
-    def Get8051Version(self):
-        temp = bytearray(20)
-        success = self.thisptr.Get8051Version(temp)
-        self._8051Version = temp.decode('utf-8').strip('\x00')
-        del temp
-        return success  # {'8051Version', self._8051Version}
+def Get8051Version(self):
+    temp = bytearray(20)
+    success = self.thisptr.Get8051Version(temp)
+    self._8051Version = temp.decode('utf-8').strip('\x00')
+    del temp
+    return success  # {'8051Version', self._8051Version}
 
-    def GetLastError(self):
-        pass
+def GetLastError(self):
+    pass
 
     # IO Pipe functions.
 
-    def SetOnPeriodOutputs(self, onPeriods):
-        cdef usbOutput temp
-        temp.byOut[0] = onPeriods[0]
-        temp.byOut[1] = onPeriods[1]
-        temp.byOut[2] = onPeriods[2]
-        temp.byOut[3] = onPeriods[3]
-        temp.byAux = onPeriods[4]
-        return self.thisptr.SetOnPeriodOutputs(temp)
+def SetOnPeriodOutputs(self, onPeriods):
+    cdef usbOutput temp
+    temp.byOut[0] = onPeriods[0]
+    temp.byOut[1] = onPeriods[1]
+    temp.byOut[2] = onPeriods[2]
+    temp.byOut[3] = onPeriods[3]
+    temp.byAux = onPeriods[4]
+    return self.thisptr.SetOnPeriodOutputs(temp)
 
-    def SetOffPeriodOutputs(self, offPeriods):
-        cdef usbOutput temp
-        temp.byOut[0] = offPeriods[0]
-        temp.byOut[1] = offPeriods[1]
-        temp.byOut[2] = offPeriods[2]
-        temp.byOut[3] = offPeriods[3]
-        temp.byAux = offPeriods[4]
-        return self.thisptr.SetOffPeriodOutputs(temp)
+def SetOffPeriodOutputs(self, offPeriods):
+    cdef usbOutput temp
+    temp.byOut[0] = offPeriods[0]
+    temp.byOut[1] = offPeriods[1]
+    temp.byOut[2] = offPeriods[2]
+    temp.byOut[3] = offPeriods[3]
+    temp.byAux = offPeriods[4]
+    return self.thisptr.SetOffPeriodOutputs(temp)
 
-    def ConfigureReelsEx(self, numberOfReels, halfStepsPerTurn, stepsPerSymbol):
-        return self.thisptr.ConfigureReelsEx(numberOfReels, halfStepsPerTurn, stepsPerSymbol)
+def ConfigureReelsEx(self, numberOfReels, halfStepsPerTurn, stepsPerSymbol):
+    return self.thisptr.ConfigureReelsEx(numberOfReels, halfStepsPerTurn, stepsPerSymbol)
 
-    def SetOutputBrightness(self, brightness):
-        return self.thisptr.SetOutputBrightness(brightness)
+def SetOutputBrightness(self, brightness):
+    return self.thisptr.SetOutputBrightness(brightness)
 
-    def SetConfig(self, port: str, config: dict, ptype: str):
-        cdef usbSerialPort _port
-        cdef _DCB* cconfig
-        cdef usbPortType _ptype
+def SetConfig(self, port: str, config: dict, ptype: str):
+    cdef usbSerialPort _port
+    cdef _DCB*cconfig
+    cdef usbPortType _ptype
 
-        cconfig.BaudRate = config['BaudRate']
-        cconfig.fParity = 1
-        cconfig.fOutxCtsFlow = 1
-        cconfig.fRtsControl = 2
-        cconfig.Parity = config['Parity']
+    cconfig.BaudRate = config['BaudRate']
+    cconfig.fParity = 1
+    cconfig.fOutxCtsFlow = 1
+    cconfig.fRtsControl = 2
+    cconfig.Parity = config['Parity']
 
+    _port = self.usbSerialPort[port]
+    _ptype = self.usbPortType[ptype]
 
-        _port = self.usbSerialPort[port]
-        _ptype = self.usbPortType[ptype]
-
-        self.thisptr.SetConfig(_port, cconfig, _ptype)
+    self.thisptr.SetConfig(_port, cconfig, _ptype)
 
     # Security pipe functions.
-    def GetPICVersion(self):
-        temp = bytearray(10)
-        success = self.thisptr.GetPICVersion(temp)
-        # Error in API with data after \0 so let fix it.
-        self.PICVersion = "".join(chr(x) for x in bytearray(temp)).split('\0', 1)[0]
-        return success  # {'PICVersion', self.PICVersion}
+def GetPICVersion(self):
+    temp = bytearray(10)
+    success = self.thisptr.GetPICVersion(temp)
+    # Error in API with data after \0 so let fix it.
+    self.PICVersion = "".join(chr(x) for x in bytearray(temp)).split('\0', 1)[0]
+    return success  # {'PICVersion', self.PICVersion}
 
     #
-    def GetPICSerialNumber(self):
-        temp = bytearray(9)
-        success = self.thisptr.GetPICSerialNumber(temp)
-        self.PICSerialNumber = "".join(chr(x) for x in bytearray(temp))
-        return success  # {'PICSerialNumber', self.PICSerialNumber}
+def GetPICSerialNumber(self):
+    temp = bytearray(9)
+    success = self.thisptr.GetPICSerialNumber(temp)
+    self.PICSerialNumber = "".join(chr(x) for x in bytearray(temp))
+    return success  # {'PICSerialNumber', self.PICSerialNumber}
 
-    def SetPICSerialNumber(self):
-        pass
+def SetPICSerialNumber(self):
+    pass
 
-    def GetDallasSerialNumber(self):
-        dallas = bytearray(8)
-        crc = bytearray(4)
-        success = self.thisptr.GetDallasSerialNumber(dallas, crc)
-        print('family code = {}'.format(dallas[0]))
-        print('Unique serial = {}.{}.{}.{}.{}.{}'.format(dallas[1], dallas[2], dallas[3], dallas[4], dallas[5],
-                                                         dallas[6]))
-        print('CRC = {}'.format(dallas[7]))
-        print('crc = {}'.format(crc))
+def GetDallasSerialNumber(self):
+    dallas = bytearray(8)
+    crc = bytearray(4)
+    success = self.thisptr.GetDallasSerialNumber(dallas, crc)
+    print('family code = {}'.format(dallas[0]))
+    print('Unique serial = {}.{}.{}.{}.{}.{}'.format(dallas[1], dallas[2], dallas[3], dallas[4], dallas[5],
+                                                     dallas[6]))
+    print('CRC = {}'.format(dallas[7]))
+    print('crc = {}'.format(crc))
 
     # Security clock
 
-    def SetClock(self, time):
-        return self.thisptr.SetClock(time)
+def SetClock(self, time):
+    return self.thisptr.SetClock(time)
 
-    def GetClock(self):
-        pass
-        #return self.thisptr.GetClock(self.security_time)
+def GetClock(self):
+    pass
+    #return self.thisptr.GetClock(self.security_time)
 
-    def RelockHardware(self):
-        return self.thisptr.RelockHardware()
+def RelockHardware(self):
+    return self.thisptr.RelockHardware()
 
     # Security switches
 
-    def NextSecuritySwitchRead(self):
-        cdef unsigned long time
-        cdef unsigned char switches
-        success = self.thisptr.NextSecuritySwitchRead(&time, &switches)
-        self.security_time = time
-        self.security_switches = switches
-        return success
+def NextSecuritySwitchRead(self):
+    cdef unsigned long time
+    cdef unsigned char switches
+    success = self.thisptr.NextSecuritySwitchRead(&time, &switches)
+    self.security_time = time
+    self.security_switches = switches
+    return success
 
-    def StartSecuritySwitchRead(self):
-        return self.thisptr.StartSecuritySwitchRead()
+def StartSecuritySwitchRead(self):
+    return self.thisptr.StartSecuritySwitchRead()
 
-    def ClearSecuritySwitches(self):
-        return self.thisptr.ClearSecuritySwitches()
+def ClearSecuritySwitches(self):
+    return self.thisptr.ClearSecuritySwitches()
 
-    def ReadAndResetSecuritySwitchFlags(self):
-        cdef unsigned char openSwitches
-        cdef unsigned char closedSwitches
-        success = self.thisptr.ReadAndResetSecuritySwitchFlags(&closedSwitches, &openSwitches)
-        self.security_closedSwitches = closedSwitches
-        self.security_openSwitches = openSwitches
-        return success
+def ReadAndResetSecuritySwitchFlags(self):
+    cdef unsigned char openSwitches
+    cdef unsigned char closedSwitches
+    success = self.thisptr.ReadAndResetSecuritySwitchFlags(&closedSwitches, &openSwitches)
+    self.security_closedSwitches = closedSwitches
+    self.security_openSwitches = openSwitches
+    return success
 
-    def CachedReadAndResetSecuritySwitchFlags(self):
-        cdef unsigned char openSwitches
-        cdef unsigned char closedSwitches
-        success = self.thisptr.CachedReadAndResetSecuritySwitchFlags(&closedSwitches, &openSwitches)
-        self.security_closedSwitches = closedSwitches
-        self.security_openSwitches = openSwitches
-        return success
+def CachedReadAndResetSecuritySwitchFlags(self):
+    cdef unsigned char openSwitches
+    cdef unsigned char closedSwitches
+    success = self.thisptr.CachedReadAndResetSecuritySwitchFlags(&closedSwitches, &openSwitches)
+    self.security_closedSwitches = closedSwitches
+    self.security_openSwitches = openSwitches
+    return success
 
     # Security battery
 
-    def ReadAndResetBatteryFailFlag(self):
-        pass
+def ReadAndResetBatteryFailFlag(self):
+    pass
 
-    def ReadAndResetRTCFailure(self):
-        pass
+def ReadAndResetRTCFailure(self):
+    pass
 
-    def EnableRandomNumberGenerator(self):
-        pass
+def EnableRandomNumberGenerator(self):
+    pass
 
     # random number generator
 
-    def GetRandomNumber(self):
-        pass
+def GetRandomNumber(self):
+    pass
 
-    def EnableRandomNumberGenerator(self):
-        pass
+def EnableRandomNumberGenerator(self):
+    pass
 
-    def DisableRandomNumberGenerator(self):
-        pass
+def DisableRandomNumberGenerator(self):
+    pass
 
-    def ReadAndResetRTCFailure(self):
-        pass
+def ReadAndResetRTCFailure(self):
+    pass
 
-    def ReadClockAtBatteryFailure(self):
-        pass
-
-    #
-    def UnlockHardware(self):
-        return UnlockX10(self.thisptr)
+def ReadClockAtBatteryFailure(self):
+    pass
 
     #
-    def UnlockHardwareRecheck(self):
-        return UnlockX10Recheck(self.thisptr)
+def UnlockHardware(self):
+    return UnlockX10(self.thisptr)
 
     #
-    def VerifyHardwareUnlockLibrary(self):
-        return VerifyX10UnlockLibrary(self.thisptr)
+def UnlockHardwareRecheck(self):
+    return UnlockX10Recheck(self.thisptr)
+
+    #
+def VerifyHardwareUnlockLibrary(self):
+    return VerifyX10UnlockLibrary(self.thisptr)
 
 
 
